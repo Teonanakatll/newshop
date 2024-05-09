d04feb16a789c2842703e5c6265ef6cfbd116c41
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2024-05-09 20:50:09 +0300

    all cart templates were included

---
+++ carts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def cart_add(request, product_id):
+    ...
+    return render()
+
+def cart_change(request, product_id):
+    ...
+    return render()
+
+def cart_remove(request, product_id):
+    ...
+    return render()
--- users/views.py
+++ users/views.py
@@ -88,3 +88,6 @@ def logout(request):
     auth.logout(request)
 
     return redirect('main:home')
+
+def users_cart(request):
+    return render(request, 'users/users_cart.html')
